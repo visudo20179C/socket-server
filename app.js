3a577ac6f5c381b732829f18018b4c966881a99d
visudo <visudo20179C@protonmail.com>
2021-09-06 18:19:32 -0400

    adds base multiplayer functionality

--- app.js
+++ app.js
@@ -13,12 +13,18 @@ const socket = require("socket.io-client")("http://172.18.154.173:8080/", {
 });
 
 io.on('connection', client => {
-	client.on('move_placed', (x,y) => {
-		console.log("A move was placed")
+	client.on('move_placed', (room,x,y) => {
+		io.to(room).emit('move_placed_received',x,y)
 	})
-	client.on('join_game', (room) => {
-		client.join(room)
-		io.to(room).emit('new_game', room)
+	client.on('join_game', (room, clientId) => {
+		if(room != client) {
+			client.join(room)
+			io.to(room).emit('new_game', room, clientId)
+		}
+	})
+	client.on('leave_game', (room) => {
+		client.leave(room)
+		io.to(room).emit('player_left')
 	})
 });
 server.listen(3000);
